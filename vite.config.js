759339ae9d38a578a2f6517af47d7d8e9792c53d
Admin <ganeshbannimatti36@gmail.com>
2026-04-10 22:52:48 +0530

    fixed vite permission issue..

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/codefiest6.0',
+  base: process.env.VITE_BASE_PATH || '/melinze26/',
 })
